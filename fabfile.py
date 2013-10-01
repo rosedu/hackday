cf54506d97b85e35f41171cdf180d84fbeab8d54
Alex Morega <alex@grep.ro>
2013-10-01 15:31:28 +0300

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,6 @@
 from fabric.api import local
 
 
-def devel():
-    local('jekyll --auto --serve')
-
-
-def build():
-    local('jekyll')
-
-
 def deploy():
-    build()
+    local('jekyll build')
     local('rsync -rtv --del _site/ hackday@rosedu.org:public_html')
